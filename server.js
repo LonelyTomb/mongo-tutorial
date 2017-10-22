228cc1b4a32d936ddb98498ce6249f14ab5cf3cd
LonelyTomb <garubav@gmail.com>
2017-10-22 22:31:06 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb');
+
+
+var db;
+var uri = "mongodb://root:password@ds229435.mlab.com:29435/mlabs-test";
+
+MongoClient.MongoClient.connect(uri, (err, database) => {
+    if (err) return console.log(err)
+    db = database
+    app.listen(3000, () => {
+        console.log('database connected')
+    })
+})
+
+const app = express();
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    db.collection('quotes').find().toArray((err, result) => {
+        if (err) return console.log(err)
+        res.render('index.ejs', { quotes: result })
+    })
+})
+
+app.post('/quotes', (req, res) => {
+    db.collection('quotes').save(req.body, (err, result) => {
+        if (err) return console.log(err)
+        console.log('saved to database')
+        res.redirect('/')
+    })
+})
